0a53f583856efc8f618784d810ff65aa9e9d5386
arteq <arteqw@gmail.com>
2013-06-26 19:39:32 +0200

    nitial  support for am33x (beaglebone black)

---
+++ target/linux/am33x/boneblack/target.mk
@@ -0,0 +1,11 @@
+#
+# Copyright (C) 2013 OpenWrt.org
+#
+
+SUBTARGET:=boneblack
+BOARDNAME:=BeagleBone Black
+
+define Target/Description
+	Build firmware images for BeagleBone Black boards.
+endef
+
